59cbe1758fde2f1036a917b418ebc08bc5e6ffdf
Kinga <niu.ula.a@poczta.fm>
2020-11-04 20:04:05 +0100

    random position in screen boundaries

--- BOIDS AT GOLDEN HOUR/Assets/Scrips/Agent.cs
+++ BOIDS AT GOLDEN HOUR/Assets/Scrips/Agent.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Agent : MonoBehaviour
 {
-   
+    public float speed;
+    
+    private void Update()
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
 
 }
--- BOIDS AT GOLDEN HOUR/Assets/Scrips/Flock.cs
+++ BOIDS AT GOLDEN HOUR/Assets/Scrips/Flock.cs
@@ -14,9 +14,10 @@ public class Flock : MonoBehaviour
     {
         for(int i=0; i<count; i++)
         {
+            Vector3 randomPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(0,Screen.height), Camera.main.farClipPlane/10));
             Agent newAgent = Instantiate(
                 prefab,
-                Random.insideUnitCircle * count,
+                randomPosition,
                 Quaternion.Euler(Vector3.forward * Random.Range(0f, 360f)),
                 transform
             );

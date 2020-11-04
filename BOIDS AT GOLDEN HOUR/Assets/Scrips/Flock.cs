7bba21cc7d3538d5a2ffcfd065c4eaccf57a1b01
Kinga <niu.ula.a@poczta.fm>
2020-11-04 17:42:25 +0100

    Init 100 agents with random position

---
+++ BOIDS AT GOLDEN HOUR/Assets/Scrips/Flock.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public int count = 100; 
+    List<Agent> agents = new List<Agent>();
+
+    public Agent prefab; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0; i<count; i++)
+        {
+            Agent newAgent = Instantiate(
+                prefab,
+                Random.insideUnitCircle * count,
+                Quaternion.Euler(Vector3.forward * Random.Range(0f, 360f)),
+                transform
+            );
+
+            newAgent.name = "agent" + i;
+            agents.Add(newAgent); 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

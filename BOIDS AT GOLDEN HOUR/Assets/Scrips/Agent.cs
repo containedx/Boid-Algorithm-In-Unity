5fe20d60874b5dd222e6834df09cb947c134c50c
Kinga <niu.ula.a@poczta.fm>
2020-11-07 14:01:07 +0100

    List Of surrounding neighbours

--- BOIDS AT GOLDEN HOUR/Assets/Scrips/Agent.cs
+++ BOIDS AT GOLDEN HOUR/Assets/Scrips/Agent.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Agent : MonoBehaviour
 {
-    public float speed;
+    public float Speed;
+
+    // current position
+    public Vector3 Position;
+
+    // current Velocity
+    public Vector3 Velocity; 
 
     // List of Neighbours - all other agents
-    public List<Agent> Neighbours = new List<Agent>(); 
+    public List<Agent> Neighbours = new List<Agent>();
+
+    // Neighbours which are in visual range 
+    public List<Agent> surroundingNeighbours = new List<Agent>();
+
+    // how far Agent can see
+    public int Distance = 5; 
 
 
     void Update()
     {
-        transform.Translate(Vector3.up * speed * Time.deltaTime);
+        // Update position
+        Position = transform.position;
+
+        //Update surrounding neighbours
+        UpdateNeighbours(); 
+
+        //transform.Translate(Vector3.up * Speed * Time.deltaTime);
+    }
+
+    public void UpdateNeighbours()
+    {
+        foreach(var neighbour in Neighbours)
+        {
+            if(Vector3.Distance(neighbour.Position, Position) <= Distance && !surroundingNeighbours.Contains(neighbour))
+            {
+                surroundingNeighbours.Add(neighbour);
+            }
+            else if(Vector3.Distance(neighbour.Position, Position) > Distance && surroundingNeighbours.Contains(neighbour))
+            {
+                surroundingNeighbours.Remove(neighbour); 
+            }
+        }
     }
 
 }

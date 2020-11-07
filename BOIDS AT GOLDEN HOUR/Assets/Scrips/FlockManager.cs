924ec8ecfd509f548055eca988a85701add42c6c
Kinga <niu.ula.a@poczta.fm>
2020-11-07 15:03:00 +0100

    Refactoring

---
+++ BOIDS AT GOLDEN HOUR/Assets/Scrips/FlockManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    // List of All Agents
+    public int count = 100; 
+    List<Boid> boids = new List<Boid>();
+
+    // Agent Prefab
+    public Boid prefab; 
+
+    void Start()
+    {
+        InitFlock();
+        CreateNeighbourhoods();
+    }
+   
+    void Update()
+    {
+        /*
+         * for each boid
+         * 
+         * apply behaviors
+         * calculate position
+         * calculate velocity
+         * 
+         * */
+    }
+
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    private void InitFlock()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Vector3 randomPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), Camera.main.farClipPlane / 10));
+            Quaternion randomRotation = Quaternion.Euler(Vector3.forward * Random.Range(0f, 360f));
+
+            Boid newBoid = Instantiate(
+                prefab,
+                randomPosition,
+                randomRotation,
+                transform
+            );
+
+            newBoid.name = "boid " + i;
+            boids.Add(newBoid);
+        }
+    }
+
+    private void CreateNeighbourhoods()
+    {
+        // Create List of Neighbours for each agent
+        foreach (var boid in boids)
+        {
+            foreach (var a in boids)
+            {
+                if (a != boid)
+                    boid.Neighbours.Add(a);
+            }
+        }
+    }
+}

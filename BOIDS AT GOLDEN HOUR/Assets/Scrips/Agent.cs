a917e6086937de311d48645c5fbdf6045ac8d5c5
Kinga <niu.ula.a@poczta.fm>
2020-11-07 13:43:53 +0100

    Neighbours List

--- BOIDS AT GOLDEN HOUR/Assets/Scrips/Agent.cs
+++ BOIDS AT GOLDEN HOUR/Assets/Scrips/Agent.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class Agent : MonoBehaviour
 {
     public float speed;
-    
-    private void Update()
+
+    // List of Neighbours - all other agents
+    public List<Agent> Neighbours = new List<Agent>(); 
+
+
+    void Update()
     {
         transform.Translate(Vector3.up * speed * Time.deltaTime);
     }
--- BOIDS AT GOLDEN HOUR/Assets/Scrips/Flock.cs
+++ BOIDS AT GOLDEN HOUR/Assets/Scrips/Flock.cs
@@ -4,32 +4,51 @@ using UnityEngine;
 
 public class Flock : MonoBehaviour
 {
+    // List of All Agents
     public int count = 100; 
     List<Agent> agents = new List<Agent>();
 
+    // Agent Prefab
     public Agent prefab; 
 
-    // Start is called before the first frame update
     void Start()
     {
         for(int i=0; i<count; i++)
         {
             Vector3 randomPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(0,Screen.height), Camera.main.farClipPlane/10));
+            Quaternion randomRotation = Quaternion.Euler(Vector3.forward * Random.Range(0f, 360f));
+
             Agent newAgent = Instantiate(
                 prefab,
                 randomPosition,
-                Quaternion.Euler(Vector3.forward * Random.Range(0f, 360f)),
+                randomRotation,
                 transform
             );
 
             newAgent.name = "agent" + i;
             agents.Add(newAgent); 
         }
-    }
 
-    // Update is called once per frame
+        // Create List of Neighbours for each agent
+        foreach(var agent in agents)
+        {
+            foreach(var a in agents)
+            {
+                if (a != agent)
+                    agent.Neighbours.Add(a); 
+            }
+        }
+    }
+   
     void Update()
     {
-        
+        /*
+         * for each boid
+         * 
+         * apply behaviors
+         * calculate position
+         * calculate velocity
+         * 
+         * */
     }
 }
